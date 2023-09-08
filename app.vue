c5fd0e652104fd4b94e36fe7f1104afa50883450
blackburn32 <alexblackburn32@gmail.com>
2023-09-07 19:12:07 -0700

    Install Nuxt Icon

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
-    <NuxtWelcome />
+      <NuxtLink class="absolute bottom-0 right-0" external="external" to="https://github.com/blackburn32/simple-synth">
+          <Icon name="uil:github" color="black" size="32" class="hover:scale-125 mr-4 mb-4"/>
+      </NuxtLink>
   </div>
 </template>
+<script setup lang="ts">
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
-
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+    devtools: {enabled: true},
+    modules: [
+        'nuxt-icon',
+    ],
+});
